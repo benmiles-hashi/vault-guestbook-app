effd0e6ebac3c048fcdce8ba3510c30707434e9b
benmiles-hashi <ben.miles@hashicorp.com>
2025-07-31 16:30:17 -0400

    updated workflow

--- terraform/vault/vault.tf
+++ terraform/vault/vault.tf
@@ -31,14 +31,41 @@ resource "vault_jwt_auth_backend_role" "ci_pipeline" {
   role_name           = "ci-pipeline"
   role_type           = "jwt"
   user_claim          = "sub"
-  bound_audiences     = ["api://Actions"]
+  bound_audiences     = ["https://github.com/benmiles-hashi"]
   bound_claims_type   = "glob"
   bound_claims = {
     repository = "benmiles-hashi/vault-guestbook-app"
   }
 
-  token_policies      = ["default"]
+  token_policies      = ["default", "demo", "sys-mounts-read"]
   token_ttl           = 1800        # 30 minutes
   token_max_ttl       = 3600        # 1 hour (optional)
   token_type          = "service"
+}
+resource "vault_policy" "kv_rw_list" {
+  name   = "kv-rw-list-all"
+  policy = <<EOT
+        # Allow full CRUD + list on all KV v2 data
+        path "secret/data/*" {
+        capabilities = ["create", "read", "update", "delete", "list"]
+        }
+
+        # Allow listing keys (metadata) in the KV mount
+        path "secret/metadata/*" {
+        capabilities = ["list"]
+        }
+    EOT
+}
+resource "vault_policy" "mounts_list" {
+  name   = "sys-mounts-read"
+  policy = <<EOT
+    # Allow listing all mounted secret engines
+    path "sys/mounts" {
+    capabilities = ["list","read"]
+    }
+    # Allow reading each mount’s details
+    path "sys/mounts/*" {
+    capabilities = ["read"]
+    }
+    EOT
 }
